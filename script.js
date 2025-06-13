780ac009df6ac14ca90b11013fbca0c184a64ed0
accio-uday <udaymanideep2002@gmail.com>
2025-06-13 16:55:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-// Complete the js code
-function Car(make, model) {}
+// Car Constructor Function
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Add getMakeModel method to Car's prototype
+Car.prototype.getMakeModel = function () {
+	return `${this.make} $ {this.model}`;
+}
 
+// SportsCar constructor Function
+function SportsCar(make, model, topSpeed) {
+	// Call car constructor to initialize make and model
+	Car.call(this, make, model);
+	this.topSpeed = topSpeed;
+}
+
+// Add getTopSpeed method to SportsCar prototype
+SportsCar.prototype.getTopSpped = function () {
+	return this.topSpeed;
+}
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
+
+
+
+
+
+
+
+
+
+
+
+
+
